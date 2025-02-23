e126e033b821a45df8138de2119535013e0622b2
Leonardo Silva <leofla@gmail.com>
2025-02-23 01:09:30 -0300

    habilitando o CORs com flask-cors para o front: http://localhost:3000

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .extensions import db, ma
 from .config import config
 from .api import pedidos_bp
@@ -6,6 +7,8 @@ from .api import pedidos_bp
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
     db.init_app(app)
     ma.init_app(app)
